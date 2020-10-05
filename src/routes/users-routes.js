c9fc6ee6603f36f2bc7fbf006b289c2e46c56662
Anderson de Oliveira <amatos.oliveira2197@gmail.com>
2020-10-05 19:09:38 -0300

    third commit

--- src/routes/users-routes.js
+++ src/routes/users-routes.js
@@ -1,14 +1,29 @@
 const usersRouter = require('express').Router();
+const User = require('../models/user');
 
-usersRouter.get('/users', (req, res) => {
-    res.send('Outro OK!')
+usersRouter.get('/users', async (req, res) => {
+    try{
+
+        const users = await User.find({ email: "anderson@gmail.com"});
+        console.log(users);
+
+    }catch(err) {
+        console.log('Erro');
+    }
+
+    res.json({
+        ok: 'ok'
+    });
 });
 
 usersRouter.post('/users', (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
 
-    
+    User.create({
+        email: email,
+        password: password
+    });
     
     res.json({
         ok: 'ok'
